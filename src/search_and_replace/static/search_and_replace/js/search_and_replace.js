27591505aea188372f1e41c60a3af7d0491faa47
Raphael Kimmig <raphael.kimmig@ampad.de>
2018-09-28 09:02:54 +0200

    ensure that only actual matches are highlighted

--- src/search_and_replace/static/search_and_replace/js/search_and_replace.js
+++ src/search_and_replace/static/search_and_replace/js/search_and_replace.js
@@ -1,14 +1,14 @@
 window.addEventListener('load', function () {
     var search_and_replace = document.getElementById("search-and-replace");
 
-    var search = new Mark(document.querySelector("div.results"));
+    var search = new Mark(document.querySelectorAll("td.search"));
     search.mark(search_and_replace.dataset.search, {
         separateWordSearch: false,
         className: "mark-search",
         caseSensitive: true
     });
 
-    var replace = new Mark(document.querySelector("div.results"));
+    var replace = new Mark(document.querySelectorAll("td.replace"));
     replace.mark(search_and_replace.dataset.replace, {
         separateWordSearch: false,
         className: "mark-replace",
